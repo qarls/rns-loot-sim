ec1bc7c7c761cf08ab480194331d73a53ef3bdf2
Carl Stephen Saul <carlstephensaul@gmail.com>
2025-11-21 00:43:08 -0600

    FInished bulk. phf hashmaps return only none.

--- src/loot.rs
+++ src/loot.rs
@@ -3,14 +3,17 @@
 /// May break if changed from 6 as of right now.
 pub static TS_COUNT: &'static usize = &6usize;
 
-/// Item Count
+/// Item Count in game
 pub static IT_COUNT: &'static usize = &200usize;
+
+/// Max items found per Treasuresphere
+pub static IT_FOUND_MAX_PER_TS: &'static usize = &5usize;
 
 /// Module to call constants based on player count
 pub mod player_loot {
     use anyhow::{bail, Error};
 
-    pub fn loot_counts(player_count: usize) -> Result<[usize; *crate::loot::TS_COUNT], Error> {
+    pub fn loot_counts(player_count: usize) -> Result<Vec<usize>, Error> {
         let loot_counts = match player_count {
             1 => *ONE,
             2 => *TWO,
@@ -21,7 +24,11 @@ pub mod player_loot {
                 player_count
             ),
         };
-        Ok(loot_counts)
+        Ok(loot_counts.to_vec())
+    }
+
+    pub fn loot_sum(player_count: usize) -> Result<usize, Error> {
+        Ok(loot_counts(player_count)?.into_iter().sum())
     }
 
     static ONE: &'static [usize; 6] = &[5, 5, 3, 3, 3, 3];
@@ -34,13 +41,15 @@ pub mod player_loot {
 // will generate a none if not found, but ehh let's go this way.
 
 pub mod treasuresphere {
-    #[allow(unused_imports)]
-    use phf::{phf_ordered_map, OrderedMap};
+    // #[allow(unused_imports)]
+    use phf::OrderedMap;
+    use phf_macros::phf_ordered_map;
 
+    // pub static IS_OPAL: Map<usize, bool> = phf_map! {
     pub static IS_OPAL: OrderedMap<usize, bool> = phf_ordered_map! {
         0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 => true,
-        120 | 121 | 122 | 123 | 124 | 125 | 126 | 127 | 128 | 129 | 130 | 131 | 132 | 133 | 134 | 135 | 136 | 137 | 138 | 139 | 140 | 141 | 142 | 143 | 144 | 145 | 146 | 147 | 148 | 149 | 150 | 151 => true,
         24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63 | 64 | 65 | 66 | 67 | 68 | 69 | 70 | 71 | 72 | 73 | 74 | 75 | 76 | 77 | 78 | 79 | 80 | 81 | 82 | 83 | 84 | 85 | 86 | 87 | 88 | 89 | 90 | 91 | 92 | 93 | 94 | 95 | 96 | 97 | 98 | 99 | 100 | 101 | 102 | 103 | 104 | 105 | 106 | 107 | 108 | 109 | 110 | 111 | 112 | 113 | 114 | 115 | 116 | 117 | 118 | 119 => false,
+        120 | 121 | 122 | 123 | 124 | 125 | 126 | 127 | 128 | 129 | 130 | 131 | 132 | 133 | 134 | 135 | 136 | 137 | 138 | 139 | 140 | 141 | 142 | 143 | 144 | 145 | 146 | 147 | 148 | 149 | 150 | 151 => true,
         152 | 153 | 154 | 155 | 156 | 157 | 158 | 159 | 160 | 161 | 162 | 163 | 164 | 165 | 166 | 167 | 168 | 169 | 170 | 171 | 172 | 173 | 174 | 175 | 176 | 177 | 178 | 179 | 180 | 181 | 182 | 183 | 184 | 185 | 186 | 187 | 188 | 189 | 190 | 191 | 192 | 193 | 194 | 195 | 196 | 197 | 198 | 199 => false,
     };
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod loot; //phf hashmaps and Vanilla game constants
-use loot::{IT_COUNT, TS_COUNT}; // vanilla constants for item count and ts count in 1.4.5
-use anyhow::{bail, Context, Error, Result};
+use anyhow::{Error, Result};
+// use anyhow::{bail, Context, Error, Result};
 use clap::Parser;
 use csv::Writer;
-use rand::{seq::SliceRandom, RngCore, SeedableRng};
+use loot::{IT_COUNT, TS_COUNT}; // vanilla constants for item count and ts count in 1.4.5
+use rand::{seq::SliceRandom, SeedableRng};
 use rand_chacha::ChaCha8Rng; // Useful for deterministic RNG
 use std::fs::File;
+use std::io::Write;
+// use std::io::prelude::Write;
 
 /// Program that simulates a thousand of games and checks if items are found
 #[derive(Parser, Debug)]
@@ -16,8 +19,8 @@ struct Args {
     run_count: u16,
 
     /// Player count
-    #[arg(short, long, default_value_t = 1, value_parser = clap::value_parser!(u8).range(1..=4))]
-    player_count: u8,
+    #[arg(short, long, default_value_t = 1, value_parser(clap::value_parser!(u64).range(1..=4)))]
+    player_count: u64,
 
     /// Output file (csv), if not used, print to stdout
     #[arg(short, long)]
@@ -26,33 +29,48 @@ struct Args {
     /// Use a positive interger (u64) seed for RNG (non-compliant)
     #[arg(short, long)]
     seed: Option<u64>,
+    // /// Use no headers
+    // #[arg(short, long, action)]
+    // no_headers: bool,
 
-    /// Use no headers
-    #[arg(short, long, action)]
-    no_headers: bool,
-
-    /// Use relative headers (i.e. in 1p, exclude it_4_{3,4} and it_5_{3,4})
-    /// 
-    /// Otherwise, default to absolute headers that make mixing mixed player
-    /// count data much easier
-    #[arg(short, long, action)]
-    relative_headers: bool,
-    }
+    // /// Use relative headers (i.e. in 1p, exclude it_4_{3,4} and it_5_{3,4})
+    // ///
+    // /// Otherwise, default to absolute headers that make mixing mixed player
+    // /// count data much easier
+    // #[arg(short, long, action)]
+    // relative_headers: bool,
+}
 
 // Probs going to impliment tokio once I confirm this working
 fn main() -> Result<(), Error> {
     let args = Args::parse();
     let game_count = args.run_count;
-    let player_count = args.player_count;
+    let player_count = args.player_count as usize;
     let mut seed = match args.seed {
         Some(val) => ChaCha8Rng::seed_from_u64(val),
         None => ChaCha8Rng::from_seed(Default::default()),
     };
 
     let mut wtr = Writer::from_writer(vec![]);
-    if !args.no_headers { field_wtr_headers(wtr, args.relative_headers, player_count); };
+    field_wtr_headers(&mut wtr, &false, &player_count)?;
+
     for _ in 0..game_count {
+        let ts: Vec<Treasuresphere> = generate_ts(&mut seed);
+        let it: Vec<&'static str> = generate_it(&ts, &mut seed, &player_count)?;
+        field_wtr(&mut wtr, &ts, &it, &false, &player_count)?;
     }
+
+    if let Some(file) = args.output_file {
+        let mut file = File::create(file)?;
+        file.write_all(&wtr.into_inner()?)?;
+    } else {
+        println!("{}", String::from_utf8(wtr.into_inner()?)?);
+    }
+
+    println!("{:?}", loot::treasuresphere::IS_OPAL.get(&6).clone());
+
+    println!("{:?}", loot::treasuresphere::ITEM_NAMES.get(&6).clone());
+
     Ok(())
 }
 
@@ -67,23 +85,83 @@ fn main() -> Result<(), Error> {
 
 /// Writes the headers for our CSV file
 ///
-/// I've included an absolute bool where for 1-3p
-/// you can include either 1p
-fn field_wtr_headers(mut wtr: Writer<Vec<u8>>, relative: bool, player_count: u8) -> Result<(), Error> {
-    // player_count +
-    let loot_count = 
-    let headers = if relative {
-        Vec::with_capacity(1 + *TS_COUNT + loot_counts.into_iter().sum() as usize)
+/// I've included an "relative" bool where for 1-3p
+/// Where in 1p, this excludes the headers  {2..=5}_{3,4}
+fn field_wtr_headers(
+    wtr: &mut Writer<Vec<u8>>,
+    _relative: &bool,
+    player_count: &usize,
+) -> Result<(), Error> {
+    let loot_counts = loot::player_loot::loot_counts(*player_count)?;
+
+    if *_relative {
+        todo!("Relative flag not priority.");
     } else {
-        Vec::with_capacity(1 + *TS_COUNT + )
-    }
-    
+        wtr.write_field("player_count")?;
+        for t in 0..*TS_COUNT {
+            let ts_t: String = format!("ts_{}", t);
+            wtr.write_field(ts_t)?;
+        }
+        for t in 0..*TS_COUNT {
+            for i in 0..*loot_counts
+                .get(t)
+                .expect("ts indexing exceeded bounds of loot_counts in field_wtr_headers().")
+            {
+                let it_t_i: String = format!("ts_{}_{}", t, i);
+                wtr.write_field(it_t_i)?;
+            }
+        }
+        wtr.write_record(None::<&[u8]>)?;
+    };
+
     Ok(())
 }
 
-fn field_wtr(mut wtr: Writer<Vec<u8>>, player_count: &u8) -> Result<(), Error> {
-    wtr.write_field(todo!())?;
-    todo!();
+fn field_wtr(
+    wtr: &mut Writer<Vec<u8>>,
+    treasurespheres: &Vec<Treasuresphere>,
+    loot: &Vec<&str>,
+    _relative: &bool,
+    player_count: &usize,
+) -> Result<(), Error> {
+    let loot_counts = loot::player_loot::loot_counts(*player_count)?;
+
+    assert_eq!(loot::player_loot::loot_sum(*player_count)?, loot.len()); // Simply checks if they're same length, should not fail
+
+    if *_relative {
+        todo!("Relative flag not priority.");
+    } else {
+        wtr.write_field(player_count.to_string())?;
+        for t in 0..*TS_COUNT {
+            let ts = treasurespheres
+                .get(t)
+                .expect("ts index exceeded the bounds of rolled treasurespheres in field_wtr().");
+            wtr.write_field(ts.to_string())?;
+        }
+
+        let mut loot_index = 0;
+        for t in 0..*TS_COUNT {
+            for i in 0..*loot::IT_FOUND_MAX_PER_TS {
+                //5
+                let loot_count = loot_counts
+                    .get(t)
+                    .expect("ts indexing exceeded bounds of loot_counts in field_wtr().");
+
+                let item = if i < *loot_count {
+                    loot_index += 1;
+                    loot.get(loot_index)
+                        .expect("Item index exceeded bounds of loot in field_wtr().")
+                } else {
+                    ""
+                }; // Write nothing i.e. for it_{2..=5}_{3,4}
+
+                wtr.write_field(item)?;
+            }
+        }
+        wtr.write_record(None::<&[u8]>)?;
+    };
+
+    Ok(())
 }
 
 /// Generates a set of random treasurespheres per game
@@ -110,13 +188,13 @@ fn generate_ts(mut seed: &mut ChaCha8Rng) -> Vec<Treasuresphere> {
 /// - in 4p, items 4_4 [24] and 5_0 [25] next to each other
 #[allow(unused_variables)]
 fn generate_it(
-    treasurespheres: Vec<Treasuresphere>,
+    treasurespheres: &Vec<Treasuresphere>,
     mut seed: &mut ChaCha8Rng,
-    player_count: u8,
+    player_count: &usize,
 ) -> Result<Vec<&'static str>, Error> {
     // Almost sure this will break if you try to put a non 6-value
-    let loot_counts: [usize; *TS_COUNT] = loot::player_loot::loot_counts(player_count)?;
-    let loot_count_sum: usize = loot_counts.into_iter::<usize>().sum() as usize;
+    let loot_counts: Vec<usize> = loot::player_loot::loot_counts(*player_count)?;
+    let loot_sum = loot::player_loot::loot_sum(*player_count)?;
 
     // Moved externally to avoid rerolling this every ts or it
     // Should always reset the counter 'p', otherwise it will be biased towards previous flavoured ts-incompatible items
@@ -126,9 +204,9 @@ fn generate_it(
     let mut itempool: Vec<usize> = (0..200).collect();
     itempool.shuffle(&mut seed);
 
-    let mut items_found = Vec::with_capacity(6);
+    let mut items_found: Vec<usize> = Vec::with_capacity(loot_sum);
     for t in 0..*TS_COUNT {
-        for loot_count in loot_counts {
+        'roll_next_item: for loot_count in &loot_counts {
             //i.e. for 1p, this goes 5,5,3,3,3,3
             let mut p: usize = 0; // Count through item indices in Pool of total itempool
             let treasuresphere = treasurespheres
@@ -136,27 +214,36 @@ fn generate_it(
                 .expect("Invalid treasuresphere indexed.");
 
             'find_valid_item: loop {
-                if treasuresphere.in_ts_pool(&p) {
-                    // Checks if item can be found in current ts, otherwise reset and advance counter
-                    for f in items_found {
+                let item: &usize = match itempool.get(p) {
+                    Some(val) => val,
+                    None => panic!(
+                        "p indexed itempool invalid value in generate_it().\nIndex value:{}",
+                        p
+                    ),
+                };
+
+                if treasuresphere.in_ts_pool(item) {
+                    //checks if in current color ts
+                    for f in &items_found {
+                        //checks if already found
                         // Checks if already prsent or not duplicate
-                        if itempool
-                            .get(p)
-                            .expect("Vector index exceeded in generate_it")
-                            != f
-                        {
-                            items_found.push(
-                                itempool
-                                    .get(p)
-                                    .expect("Items found push in generate_it failed"),
-                            ); // Add item to our items found on unique
-                            p += 1; // advance item counter in current treasuresphere
+                        if item != f {
+                            continue;
+                        } else {
+                            p += 1;
+                            continue 'find_valid_item;
                         }
+                        // Add item to our items found on unique after finishing loop
                     }
-                } else {
-                    p += 1; // advance item counter in current treasuresphere until item in treasuresphere is found
-                    continue 'find_valid_item;
+
+                    p += 1;
+                    items_found.push(
+                        *itempool
+                            .get(p)
+                            .expect("Items found push in generate_it failed"),
+                    );
                 }
+                continue 'roll_next_item;
             }
         }
     }
@@ -197,18 +284,22 @@ impl Treasuresphere {
 
     fn in_ts_pool(&self, loot: &usize) -> bool {
         let option = match &self {
-            Self::Normal if *loot < IT_COUNT.clone() as usize => return true,
-            Self::Normal => panic!("Loot index is out of bounds: {}", loot),
-            Self::Opal => loot::treasuresphere::IS_OPAL.get(loot),
-            Self::Sapphire => loot::treasuresphere::IS_SAPPHIRE.get(loot),
-            Self::Ruby => loot::treasuresphere::IS_RUBY.get(loot),
-            Self::Garnet => loot::treasuresphere::IS_GARNET.get(loot),
-            Self::Emerald => loot::treasuresphere::IS_EMERALD.get(loot),
+            Treasuresphere::Normal if *loot < IT_COUNT.clone() as usize => return true,
+            Treasuresphere::Normal => panic!("Loot index is out of bounds: {}", loot),
+            Treasuresphere::Opal => loot::treasuresphere::IS_OPAL.get(loot).clone(),
+            Treasuresphere::Sapphire => loot::treasuresphere::IS_SAPPHIRE.get(loot).clone(),
+            Treasuresphere::Ruby => loot::treasuresphere::IS_RUBY.get(loot).clone(),
+            Treasuresphere::Garnet => loot::treasuresphere::IS_GARNET.get(loot).clone(),
+            Treasuresphere::Emerald => loot::treasuresphere::IS_EMERALD.get(loot).clone(),
         };
 
         match option {
-            None => panic!("Loot index out of bounds: {}", loot),
             Some(val) => return *val,
+            None => panic!(
+                "Loot not found at index: {}\nTreasuresphere:{}\nfunction: in_ts_pool()",
+                loot,
+                self.to_string()
+            ),
         }
     }
 }
